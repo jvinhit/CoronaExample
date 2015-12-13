c8f56765689877a87b452c1be96b652bdd4377b0
jvinhit <jvinhit@gmail.com>
2015-12-13 21:20:06 +0700

    Signed-off-by: jvinhit <jvinhit@gmail.com>

---
+++ Images/newImageTutorial/main.lua
@@ -0,0 +1,9 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+local background = display.newImage("images/background_with_border.png",display.contentWidth/2,display.contentHeight/2)
